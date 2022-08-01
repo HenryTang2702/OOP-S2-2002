be63d5a16b7c1c2b0dd515ac4fc81c815eb5ebbe
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-02 01:14:28 +0930

    Add new one

---
+++ main_transform.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <iostream>
+
+
+using namespace std;
+void show_binary( int dec );
+
+int main(){
+    int number = 24;
+    show_binary(number);
+
+    return 0;
+   
+}
+
+
+
