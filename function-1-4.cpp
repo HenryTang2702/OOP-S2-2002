3c773451e96320be400eed9051739c1b3bddd605
HenryTang2702 <109932321+HenryTang2702@users.noreply.github.com>
2022-08-04 01:43:37 +0930

    Update function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 void print_scaled(int array[3][3],int scale){
-
-    for(int i=0; i<3;i++){
-        for(int j=0; j<3; j++){
-            array[i][j] *= scale;
-              std::cout<<array[i][j]<<" ";       
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            std::cout<<array[i][j]*scale<<' ';
         }
-        std::cout<<"\n";
+        std::cout<<std::endl;
     }
 }
