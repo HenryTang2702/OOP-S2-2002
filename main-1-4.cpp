957d881b301622bb88ffd477781295f4f7de2438
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-04 01:13:50 +0930

    files 4

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 
 using namespace std;
+void print_scaled(int array[3][3],int scale);
 
 int main(){
+    int scale = 3 ;
+    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
+    print_scaled(threebythree,scale) ;
 
     return 0;
 }
+
+// // 0 3 6
+// 9 12 15
+// 18 21 24
