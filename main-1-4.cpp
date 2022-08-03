043845e84d35bc1c61de326109dea31275a86125
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-04 01:25:07 +0930

    hello

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -4,13 +4,9 @@ using namespace std;
 void print_scaled(int array[3][3],int scale);
 
 int main(){
-    int scale = 3 ;
-    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
-    print_scaled(threebythree,scale) ;
+    int scale = 7 ;
+    int a[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
+    print_scaled(a,scale) ;
 
     return 0;
 }
-
-// // 0 3 6
-// 9 12 15
-// 18 21 24
