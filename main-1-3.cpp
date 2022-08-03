25695967ac2e6ce8421cbf1d7f7ee49ee9715947
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-03 22:37:28 +0930

    Add all files

---
+++ main-1-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+void count_digits(int array[4][4]);
+
+int main(){
+     int a[4][4] = {{1,2,3,2},
+                   {1,2,3,2},
+                   {1,2,3,2}, 
+                   {1,2,3,4}};
+    count_digits(a);
+    return 0;
+}
--- transform.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main(){
-    cout<<"hello world"<<endl;
-    cout << "Anh Huy la so mot"<<endl;
-    return 0;
-}
