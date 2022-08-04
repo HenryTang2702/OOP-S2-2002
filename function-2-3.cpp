adb9506c6d09e08ddb55e44ef7306cd83d090b89
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-05 02:03:15 +0930

    Add file 3 and 4

---
+++ function-2-3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int sum_if_palindrome(int integers[], int length){
+
+}
+bool is_palindrome(int integers[], int length){
+
+}
+int sum_array_elements(int integers[], int length){
+    
+}
--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 
 using namespace std;
+int sum_min_max(int integers[], int length);
 
 int main(){
+    int a[5] = {1,3,5,7,9};
+    cout << sum_min_max(a, 5);
+
 
     return 0;
 }
