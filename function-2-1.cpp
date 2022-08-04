3a3a4d287da24e7bc6f81ea5797c6019d6dbf164
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-05 01:20:18 +0930

    binary

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,4 +1,21 @@
+
+#include<iostream>
+
+#include<string>
 void print_binary_str(std::string decimal_number){
+    int number = stoi(decimal_number);
+	int rem=0;
+	int base=1;
+	while(number!=0)
+	{
+		rem = rem + (number%2)*base;
+		base*=10;
+		number/=2;
+	}
+	
+	std::cout<<rem<<std::endl;
+
+
 
     
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 void print_binary_str(std::string decimal_number);
 
 int main(){
-
+    print_binary_str("9");
     return 0;
 }
