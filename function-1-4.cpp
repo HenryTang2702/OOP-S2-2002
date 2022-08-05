1337598a6e8d91f9c533dd4747760db768a801a4
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-05 13:12:31 +0930

    fuc k space

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,12 @@ void print_scaled(int array[3][3],int scale){
     for(int i=0; i<3;i++){
         for(int j=0; j<3; j++){
             array[i][j] *= scale;
-              std::cout<<array[i][j]<<" ";       
+            if(j == 2){
+                std::cout<<array[i][j];
+            }
+            else{
+              std::cout<<array[i][j]<<" ";
+            }       
         }
         std::cout<<"\n";
     }
