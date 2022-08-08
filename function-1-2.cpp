66062a8b62b1b37e27d0eed8daa73f39f91bdd88
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-08 12:31:53 +0930

    2

---
+++ function-1-2.cpp
@@ -0,0 +1,3 @@
+void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents){
+    
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents);
+
+
+
+int main(){
+
+
+    return 0;
+}
