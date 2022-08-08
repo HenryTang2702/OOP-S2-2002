e38d8408e41c5ac44884eccfc7b7fd567278b40f
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-08 12:11:25 +0930

    no line

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,1 +1,14 @@
-#include
+#include<iostream>
+void printer(int array[10][10]){
+
+for(int i =0; i< 10; i++){
+    for(int j =0; j< 10; j++){
+        if(j ==10){
+            std::cout<<array[i][j]<<"";
+        }
+        std::cout << array[i][j] <<" ";
+}
+std::cout <<std::endl;
+
+}
+}
