550fecb9e6de98fb57c728a4f1e1b56e250e4f47
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-10 13:37:38 +0930

    all done

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -6,8 +6,8 @@ bool is_array_palindrome(int integers[], int length);
 
 
 int main(){
-     int a[5];
-     cout<< palindrome_sum(a, 0);
+     int a[5] = {1,2,2,2,1};
+     cout<< palindrome_sum(a, 5);
      
     return 0;
 }
