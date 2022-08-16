2e311056ccd49e7ffc279f3253e41ca85b82b9d9
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-17 01:38:53 +0930

    Hello gain

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,8 +3,9 @@ using namespace std;
 void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 int main()
 {
-string a = "a";
-int sie = sizeof(string);
-cout<< sie<<endl;
+string first[3][2] = { "One" , "Two" ,"Three" ,"Four" ,"Five" ,"Six" };
+string second[3][2];
+int n=3;
+string_2d_copy(first, second, n) ;
 return 0;
 }
