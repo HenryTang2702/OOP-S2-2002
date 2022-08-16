88327ceac424787fe92294c5968b63516473a6d0
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-17 00:42:45 +0930

    helllo

---
+++ function-1-1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+void string_2d_copy(std::string first[][2], std::string second[][2], int n){
+
+
+    
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void string_2d_copy(std::string first[][2], std::string second[][2], int n);
+int main()
+{
+string a = "a";
+int sie = sizeof(string);
+cout<< sie<<endl;
+return 0;
+}
