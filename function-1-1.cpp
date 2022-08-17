acd2bf2a62149bcfd7fac23a13a8553a13c967d8
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-17 13:30:50 +0930

    haha

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,11 +4,6 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
     for (int i=0; i<n; i++) {
         for (int j=0; j<2; j++) {
             second[i][j] = first[i][j];
-            std::cout<<second[i][j]<<" ";
 }
-std::cout<<std::endl;
 }
-
-
-
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,5 +8,12 @@ string array1[3][2] = { {"1" , "1"} ,{"2" ,"2"} ,{"3" ,"3"} };
 string array2[3][2];
 int n=3;
 string_2d_copy(array1, array2, n) ;
-return 0;
+ for (int i=0; i<n; i++) {
+        for (int j=0; j<2; j++) {
+            cout<<array2[i][j]<<" ";
+
+        }
+        cout<<endl;
+ }
+        return 0;
 }
