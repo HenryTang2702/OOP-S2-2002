54a88c193e00d0fbff54d43bd155fe5ca211ee10
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-17 13:25:42 +0930

    a

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 int main()
 {
-string array1[3][2] = { {"One" , "Two"} ,{"Three" ,"Four"} ,{"Five" ,"Six"} };
+string array1[3][2] = { {"1" , "1"} ,{"2" ,"2"} ,{"3" ,"3"} };
 string array2[3][2];
 int n=3;
 string_2d_copy(array1, array2, n) ;
