c1b052a3bee535acaa1985682a1cbfdb62736223
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-17 14:00:11 +0930

    question 2

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,7 +5,14 @@ using namespace std;
 void copy_integers(int old_array[],int new_array[],int length);
 
 int main() {
-
+    int *ptr;
+    int a[5] ={1,3,5,7,9}; 
+    int b[5];
+    int n =5;
+    copy_integers(a, b, n);
+    for ( int i =0; i < n; i++){
+        cout<< b[i] <<" ";
+    }
 
     return 0;
 }
