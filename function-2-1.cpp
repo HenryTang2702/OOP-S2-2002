f8623f00d09a57813f534b11e73dde87697cc944
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-18 15:41:46 +0930

    add 2.1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,5 +1,8 @@
+#include<iostream>
 void multiples_of_seven(int *nums,int length){
     for(int i =0; i< length; i++){
-        *(nums + i) *= 7;
+        if(*(nums + i) % 7 == 0){
+            std::cout<<*(nums+i)<<std::endl;
+        }
     }
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,13 +5,9 @@ using namespace std;
 void multiples_of_seven(int *nums,int length);
 
 int main() {
-    int a[5] = {1, 3, 5, 7, 9};
+    int a[5] = {1, 14, 5, 7, 9};
     int *ptr = a;
     int n =5;   
     multiples_of_seven(ptr,n);
-    for(int i =0; i< n; i++){
-
-        cout<< a[i]<<endl;
-    }
     return 0;
 }
