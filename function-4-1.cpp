7e6c7454236e4668ae106e83a463c02d9f891b17
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-24 14:56:03 +0930

    dsad

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -2,10 +2,24 @@
 
 int* matrix_min_max(int **vals,int num_rows, int num_cols){
     int *min_max = new int[2];
+    for(int i = 0; i< 2; i++){
+        min_max[i] = vals[0][0];
+    }
     
     for(int i =0; i < num_rows; i++){
         for(int j =0; j < num_rows; j++){
-
+            vals[i][j] < min_max[0];
+            min_max[0]= vals[i][j];
         }
     }
-}
+
+      for(int i =0; i < num_rows; i++){
+        for(int j =0; j < num_rows; j++){
+            vals[i][j] > min_max[1];
+            min_max[0]= vals[i][j];
+        }
+        
+
+        }
+        return min_max;
+    }
