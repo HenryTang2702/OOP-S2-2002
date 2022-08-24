5ca0c877b6d953f5e17964f7918763b3ae79ef0b
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-24 14:07:03 +0930

    a

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,26 @@
 #include<iostream>
 void print_ascending(int *vals, int len){
 
-    for(int i =1; i < len; i++){
-        if(vals[i -1] < vals[i]){
-        std::cout<<vals[i]<<" ";
+     for (int i = 0; i < len; i++){
+        for (int j = i; j < len; j++){
+            if (vals[i] > vals[j+1]){
+                int temp = vals[i];
+                vals[i] = vals[j+1];
+                vals[j+1] = temp;
+            }
         }
         
     }
+    for(int i =0; i < len; i++){
+        std::cout<<vals[i]<<" ";
+    }
+    // for(int i =1; i < len; i++){
+    //     if(vals[i -1] < vals[i]){
+    //     std::cout<<vals[i-1]<<" ";
+    //     }
+    //     else{
+    //         std::cout<<"";
+    //     }
+        
+    // }
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void print_ascending(int *vals, int len);
 int main(){
-    int vals[]={1,2,3,4,5,4,3,2,1, 7,8};
+    int vals[]={1,2,3,4,5,4,3,2,1,7,8};
     print_ascending(vals, 11);
 return 0;
 }
