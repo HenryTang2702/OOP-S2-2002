a81d71479b1cd51acc9228440364baa56f37a3ea
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-24 14:49:48 +0930

    hsa

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
 int* matrix_min_max(int **vals,int num_rows, int num_cols){
-
+    int *min_max = new int[2];
     
+    for(int i =0; i < num_rows; i++){
+        for(int j =0; j < num_rows; j++){
+
+        }
+    }
 }
