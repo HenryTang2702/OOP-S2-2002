064e4c99d5545a606e03688c4b85392d6049a62a
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-24 14:12:56 +0930

    hello

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
-#include<string.h>
-#include<stdlib.h>
-
 using namespace std;
-string month_lookup(int month){
-    if(month == 1){
-        return June;
-    }
 
+//array to store elements 
+string arr[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+string month_lookup(int month){
+
+    string str =" ";
+
+    if(month >= 1 && month <= 12)
+        str = arr[month-1];
+    //otherwise it is invalid
+    else
+        str = "invalid month";
+
+    //return
+    return str;
 
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,9 +5,6 @@ using namespace std;
 string month_lookup(int month);
 
 int main(){
-    cout<<month_lookup(1);
-
-
-
+    cout<<month_lookup(5);
     return 0;
 }
