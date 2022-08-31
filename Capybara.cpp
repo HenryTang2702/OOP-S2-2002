45f60a85738478c2f8bec08ce8f97dfb5731bf40
HenryTang2702 <copsuilat1237890@gmail.com>
2022-08-31 17:12:26 +0930

    dsa

--- Capybara.cpp
+++ Capybara.cpp
@@ -2,20 +2,26 @@
 #include<string>
 #include "Capybara.h"
 #include<stdio.h>
+#include<iostream>
 
 using namespace std;
 
-Capybara::Capybara(){}
-void Capybara::setName(string capyName){
-        name = capyName;
-    }   // change the capybara's name
-string Capybara:: getName(){
+Capybara::Capybara(){
+
+}
+string Capybara::getName(){
     return name;
 }
+void Capybara::setName(string capyName){
+    name = capyName;
+    }   // change the capybara's name
+
 void Capybara::setAge(int capyAge){
     age = capyAge;
 }      // change the capybara's age
 int Capybara:: getAge(){
     return age;
 }
-Capybara:: ~Capybara(){}
+Capybara:: ~Capybara(){
+    
+}
--- Capybara.h
+++ Capybara.h
@@ -7,7 +7,7 @@ class Capybara{
     int age;
 public:
     Capybara() ;                     // no name or age is required to create a Capybara
-    void setName(std::string capyName);   // change the capybara's name
+    void setName(string capyName);   // change the capybara's name
     string getName();
     void setAge(int capyAge);        // change the capybara's age
     int getAge();
