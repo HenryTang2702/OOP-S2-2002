1bb772d1b82e0b4c44082aff2e9fa0f69fe835fc
HenryTang2702 <copsuilat1237890@gmail.com>
2022-09-01 11:56:18 +0930

    ds

--- Person.cpp
+++ Person.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
+#include<stdio.h>   
 #include<string.h>
 #include<string>
+#include "Person.h"
 
 using namespace std;
 
-int main() {
-
-    return 0;
+Person::Person(){};
+Person::Person(int mySalary, string myName){
+    salary = mySalary;
+    name = myName;
 }
+string Person::getName(){
+    return name;
+}
+void Person:: setName(string myName){
+    name = myName;
+}
+int Person:: getSalary(){
+    return salary;
+}
+void Person:: setSalary(int mySalary){
+    salary = mySalary;
+}
+
+
+
+
--- Person.h
+++ Person.h
@@ -1,12 +1,19 @@
 #include<iostream>
 #include<string.h>
 #include<string>
+#ifndef PERSON_H
+#define PERSON_H
 
 using namespace std;
 class Person{
+public:
+    int salary;
+    string name;
+Person();
 Person(int mySalary, string myName); // a name and salary must be provided to create a Person
 void setName(string myName);         // change the Person's name
 string getName();
 void setSalary(int mySalary);        // change the Person's salary
 int getSalary();
 };
+#endif 
