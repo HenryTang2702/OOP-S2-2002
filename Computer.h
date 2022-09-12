3537de817fc762e1b1b4cc2d861303f67118bb21
HenryTang2702 <copsuilat1237890@gmail.com>
2022-09-12 12:09:55 +0930

    aaa

--- Computer.h
+++ Computer.h
@@ -1,5 +1,9 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
+#include<iostream>
+#include<string>
+
+using namespace std;
 
 #pragma once
 
@@ -7,10 +11,18 @@ class Computer
 {
 public:
     Computer();
+    Computer(string letter); // set what move the computer will
+                             // make (rock, paper, or scissors)
+                             // if the input is not r, R, p, P, s, S or
+                             // a string starting with one of these letters,
+                             // set the move to the default ‘r’
+
+    string getMoves(); // returns all moves stored in a string
+
+    void move(); // increments number of moves made
     ~Computer();
 
 private:
-
 };
 
 #endif
--- Person.h
+++ Person.h
@@ -1,16 +1,26 @@
 #ifndef PERSON_H
 #define PERSON_H
+#include<iostream>
+#include<string>
 
-#pragma once
+using namespace std;
 
 class Person
 {
+protected:
+    char *moves;
 public:
     Person();
+    void move(); // allow user to type in a single character to
+                 // represent their move. If a move is impossible,
+                 //“Move unavailable” is outputted and the user is
+                 // asked to input a character again.
+                 // Otherwise, their input is stored
+
+    string getMoves(); // returns all moves stored in a string
     ~Person();
 
 private:
-
 };
 
 #endif
