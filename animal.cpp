c2beb616b1052594bf4b56386caeda5de15d5620
HenryTang2702 <copsuilat1237890@gmail.com>
2022-10-24 12:16:51 +1030

    huydeptrai

---
+++ animal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include"animal.h"
+
+using namespace std;
+
+animal ::animal(string n, int v)
+{
+    animalID++;
+    name = n;
+
+    volume = v;
+}
+
+// Define the member functions.
+void animal::set_name(string n)
+{
+
+    name = n;
+}
+void animal::set_volume(int v)
+{
+
+    volume = v;
+}
+int animal::get_volume()
+{
+
+    return volume;
+}
+int animal::get_animalID()
+{
+
+    return animalID;
+}
--- animal.h
+++ animal.h
@@ -1,14 +1,33 @@
 #include <iostream>
-
 using namespace std;
+
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#pragma once
 
 class animal
 {
+protected:
+    string name;
+    int animalID;
+    int volume;
 
-}
+public:
+    // Declare the constructor.
+    animal(string n, int v);
 
-animal(string n, int v); // creates an animal with name n and body volume v.
-                         // animals are allocated a unique ID on creation
-string name;             // the animal's name
-int animalID;            // the animal's unique ID
-int volume;              // the volume of the animal's body
+    // Define the pure virtual function.
+    virtual string get_name() = 0;
+
+    // Declare the member functions.
+    void set_name(string n);
+
+    void set_volume(int v);
+
+    int get_volume();
+
+    int get_animalID();
+};
+
+#endif
