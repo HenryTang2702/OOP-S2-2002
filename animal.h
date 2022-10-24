fb752a47ef008964d9323b259efadb9a583f4cb2
HenryTang2702 <copsuilat1237890@gmail.com>
2022-10-24 11:47:43 +1030

    das

--- animal.h
+++ animal.h
@@ -1,9 +1,14 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-animal(string n, int v) ;  // creates an animal with name n and body volume v.
-                           // animals are allocated a unique ID on creation
-string name ;              // the animal's name
-int animalID ;             // the animal's unique ID
-int volume ;               // the volume of the animal's body
+class animal
+{
+
+}
+
+animal(string n, int v); // creates an animal with name n and body volume v.
+                         // animals are allocated a unique ID on creation
+string name;             // the animal's name
+int animalID;            // the animal's unique ID
+int volume;              // the volume of the animal's body
