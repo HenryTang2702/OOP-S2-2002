35a857689987a881948b97153362f721cca0c943
HenryTang2702 <copsuilat1237890@gmail.com>
2022-10-24 11:44:18 +1030

    sdasda

---
+++ animal.h
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+animal(string n, int v) ;  // creates an animal with name n and body volume v.
+                           // animals are allocated a unique ID on creation
+string name ;              // the animal's name
+int animalID ;             // the animal's unique ID
+int volume ;               // the volume of the animal's body
